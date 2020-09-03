e5877d5ae5c8599c9d1de4afd1d46a644c01f99a
Olivier Fitoussi <o_fitoussi@yahoo.fr>
2020-09-03 17:43:28 +0200

    modifs Audelia 1744HRS

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,18 +4,24 @@
 #
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
-module Admin
-  class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
+#module Admin
+  #class ApplicationController < Administrate::ApplicationController
+    #before_action :authenticate_admin
 
-    def authenticate_admin
+    #def authenticate_admin
       # TODO Add authentication logic here.
-    end
+    #end
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
     # def records_per_page
     #   params[:per_page] || 20
     # end
+  #end
+#end
+
+module Admin
+  class Admin::ApplicationController < Administrate::ApplicationController
+    http_basic_authenticate_with name: ENV.fetch("ADMIN_NAME"), password: ENV.fetch("ADMIN_PASSWORD")
   end
 end
